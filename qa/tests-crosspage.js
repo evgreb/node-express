69572d119c1ec4c43750d931fc1fa0e03a943189
Евгений Гребенщиков <evgreb@gmail.com>
2018-06-10 13:45:34 +0300

    tests config

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -1,11 +1,24 @@
-describe('Межстраничные тесты', function() {
+const puppeteer = require('puppeteer');
+const {expect} = require('chai');
+
+const opts = {
+  headless: false,
+  slowMo: 100,
+  timeout: 10000,
+};
+
+describe('Межстраничные тесты', async () => {
   let page;
+  let browser;
+
   before(async () => {
+    browser = await puppeteer.launch(opts);
     page = await browser.newPage();
   });
 
   after(async function() {
     await page.close();
+    browser.close();
   });
 
   it('Good River: Должно заполняться поле реферера', async function() {
